2056f051745de42b029cac63c1add9b0f9a1c907
giovanna.santos <giovanna.santos@marketup.com>
2022-01-26 17:50:31 -0300

    Subindo arquivos / Ainda não está responsivo

---
+++ index.js
@@ -0,0 +1,39 @@
+var crl = document.getElementById('myCrl').getContext('2d');
+
+        crl.beginPath();
+        crl.arc(60, 100, 40, 0, 2 * Math.PI);
+        crl.fillStyle = '#ffffe0';
+        crl.fill();
+        crl.linewidth = 5;
+        crl.strokeStyle = '#dbdbdb';
+        crl.stroke();
+
+        var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+        crl1.beginPath();
+        crl1.arc(60, 100, 40, 0, 2 * Math.PI);
+        crl1.fillStyle = '#90ee90';
+        crl1.fill();
+        crl1.linewidth = 5;
+        crl1.strokeStyle = '#dbdbdb';
+        crl1.stroke();
+
+        var crl2 = document.getElementById('myCrl2').getContext('2d');
+
+        crl2.beginPath();
+        crl2.arc(60, 100, 40, 0, 2 * Math.PI);
+        crl2.fillStyle = '#add8e6';
+        crl2.fill();
+        crl2.linewidth = 5;
+        crl2.strokeStyle = '#dbdbdb';
+        crl2.stroke();
+
+        var crl3 = document.getElementById('myCrl3').getContext('2d');
+
+        crl3.beginPath();
+        crl3.arc(60, 100, 40, 0, 2 * Math.PI);
+        crl3.fillStyle = '#f08080';
+        crl3.fill();
+        crl3.linewidth = 5;
+        crl3.strokeStyle = '#dbdbdb';
+        crl3.stroke();
